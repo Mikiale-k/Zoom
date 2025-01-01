b11c8ef13b136611763cdaf200d19ddd0c5054bc
Mikialekesate <mikimkmnkmiki@gmail.com>
2025-01-01 11:45:05 +0300

    finished...

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import CallList from "@/components/CallList";
+import React from "react";
 
 const Recordings = () => {
   return (
     <section className="flex flex-col size-full gap-10 text-white">
       <h1 className="text-3xl font-bold">Recordings</h1>
-    </section>
-  )
-}
 
-export default Recordings
+      <CallList type="recordings" />
+    </section>
+  );
+};
+
+export default Recordings;
